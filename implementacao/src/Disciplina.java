e54a04e737d42a416fc129b4cd6b8b49208824a7
João Vitor Neri Moreira <130504908+j0a0N3ri@users.noreply.github.com>
2025-08-26 12:02:22 -0300

    Update Disciplina.java

--- implementacao/src/Disciplina.java
+++ implementacao/src/Disciplina.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.io.EOFException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,18 +8,89 @@ public class Disciplina {
     
     private String nome;
     private String codigo;
-    private static final int maxAlunos = 60;
-    private static final int minAlunos = 3;
     private boolean obrigatorio;
     private List<Aluno> alunos;
+    private Professor professor;
+    private int qtdAlunos;
+    private boolean podeMatricular;
 
-    public Disciplina(String nome, String codigo, boolean obrigatorio) {
+    
+
+    private static final int maxAlunos = 60;
+    private static final int minAlunos = 3;
+
+    public Disciplina(String nome, String codigo, boolean obrigatorio,Professor professor) {
         this.nome = nome;
         this.codigo = codigo;
         this.obrigatorio = obrigatorio;
         this.alunos = new ArrayList<>();
+        this.professor = professor;
     }
 
+    public boolean validarDisciplina(){
+
+        boolean validada = false;
+
+        if(qtdAlunos > minAlunos)
+            validada = true;
+        
+        return validada;
+
+    }
+
+    public boolean temVaga(){
+
+        boolean temVaga = false;
+
+        if (qtdAlunos < maxAlunos)
+            temVaga = true;
+        
+        return temVaga;
+
+    }
+
+    public void adicionarAluno(Aluno aluno) throws Exception{
+
+        if (temVaga() && verificarPeriodo()){
+            alunos.add(aluno);
+            qtdAlunos++;
+        }else{
+            throw new Exception("Não e possivel se matricular!");
+        } 
+
+    }
+
+    public void removerAluno(Aluno aluno) throws Exception{
+
+        if(alunos.remove(aluno)){
+            qtdAlunos--;
+            return;
+        }else{
+            throw new Exception("Aluno não existe nesta disciplina!");
+        }
+
+    }
+
+    public boolean verificarPeriodo(){
+        
+        boolean permitido = false;
+
+        if (podeMatricular)
+            permitido = true;
+        
+        return permitido;
+
+    }
+
+    //--------------------------------------------------
+
+    public boolean isPodeMatricular() {
+        return podeMatricular;
+    }
+
+    public void setPodeMatricular(boolean podeMatricular) {
+        this.podeMatricular = podeMatricular;
+    }
     public String getNome() {
         return nome;
     }
@@ -51,25 +123,6 @@ public class Disciplina {
         this.alunos = alunos;
     }
 
-    public boolean validarDisciplina(){
-        //stub
-        return true;
-    }
-
-    public void adicionarAluno(Aluno aluno){
-        //stub
-    }
-
-    public void removerAluno(Aluno aluno){
-        //stub
-    }
-
-    public boolean verificarPeriodo(){
-        //stub
-        return true;
-    }
-
-    
 }
 
 
